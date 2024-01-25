97f1fa4520c559048d9cf2aee166ef80aefc42a2
ka-65z <148937965+ka-65z@users.noreply.github.com>
2024-01-25 13:57:56 +0900

    Update storage.js
    row 4 new s3client ({region: REGION})

--- routes/storage.js
+++ routes/storage.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { ListBucketsCommand, S3Client, ListObjectsV2Command, GetObjectCommand } from "@aws-sdk/client-s3";
 const router = express.Router();
-const client = new S3Client({});
+const client = new S3Client({region: REGION});
 
 // GET 
 //Expres ルーター動作確認
